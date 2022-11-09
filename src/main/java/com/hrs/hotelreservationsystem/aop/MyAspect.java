f69056028b972fe7caac5924ec5424e9ebc7b9bc
Dennywong-java <109556676+Dennywong-java@users.noreply.github.com>
2022-11-09 15:05:03 -0500

    Update MyAspect.java

--- src/main/java/com/hrs/hotelreservationsystem/aop/MyAspect.java
+++ src/main/java/com/hrs/hotelreservationsystem/aop/MyAspect.java
@@ -16,15 +16,29 @@ public class MyAspect {
     private Logger logger = LoggerFactory.getLogger("MyAspect Loggers");
 
     @Pointcut("execution(public * com.hrs..*(..))")
-    public void pointCut(){
+    public void pointCut() {
 
     }
 
     @AfterThrowing(pointcut = "pointCut()", throwing = "myException")
-    public void doHandlerException(JoinPoint joinPoint, Exception myException){
+    public void doHandlerException(JoinPoint joinPoint, Exception myException) {
         Signature signature = joinPoint.getSignature();
         String methodName = signature.getName();
         logger.error(String.format("AfterThrowing: Method %s is throwing errors: %s", methodName, myException.getMessage()));
     }
+
+
+    public static void main(String[] args) {
+        String s = "abc";
+        String s2= s + "sdds";
+        System.out.println(s2);
+        String temp ="";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            System.out.println(s.charAt(i));
+            temp += s.charAt(i);
+        }
+        System.out.println(temp);
+    }
+
 
 }
